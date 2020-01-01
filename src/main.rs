8d38cf05e2dcbf76bb34fdb9182d3050bcf7a702
xorphitus <xorphitus@gmail.com>
2020-01-01 16:44:26 +0900

    wip refactor

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
+#[macro_use]
+extern crate lazy_static;
+
+#[cfg(target_os = "linux")]
 use std::fs::File;
 use std::io;
+#[cfg(target_os = "linux")]
 use std::io::BufRead;
+#[cfg(target_os = "linux")]
 use std::io::BufReader;
 use std::io::Read;
 use std::io::Write;
 use std::net::{TcpListener, TcpStream};
+#[cfg(target_os = "macos")]
+use std::process::Command;
 use std::sync::mpsc;
 use std::sync::mpsc::Sender;
 use std::thread;
@@ -15,6 +23,16 @@ use threadpool::ThreadPool;
 
 const BUF_SIZE: usize = 1024;
 const DEFAULT_WORKER_NUM: usize = 1;
+
+#[cfg(target_os = "linux")]
+lazy_static! {
+    static ref CPU_RE: Regex = Regex::new(r"^processor\s+.+$").unwrap();
+}
+
+#[cfg(target_os = "macos")]
+lazy_static! {
+    static ref CPU_RE: Regex = Regex::new(r"^\s*Number of Processors:\s*(\d+)\s*$").unwrap();
+}
 
 fn handle_client(mut stream: TcpStream, log_tx: &Sender<String>) -> Result<(), Error> {
     loop {
@@ -32,21 +50,33 @@ fn handle_client(mut stream: TcpStream, log_tx: &Sender<String>) -> Result<(), E
     }
 }
 
+#[cfg(target_os = "linux")]
 fn get_cores() -> Result<usize, Error> {
     let path = "/proc/cpuinfo";
     let f = File::open(path)?;
-    // Using `unwrap`: this is reasonable.
-    // Since this program should be terminated ASAP when the regexp is incorrect.
-    let re = Regex::new(r"^processor\s+.+$").unwrap();
 
     let file = BufReader::new(&f);
     let mut i = 0;
     for line in file.lines() {
-        if re.is_match(&line?) {
+        if CPU_RE.is_match(&line?) {
             i += 1;
         }
     }
     Ok(i)
+}
+
+#[cfg(target_os = "macos")]
+fn get_cores() -> Result<usize, Error> {
+    let sys_prof = Command::new("system_profiler")
+        .arg("SPHardwareDataType")
+        .output()?;
+    let sys_prof = String::from_utf8(sys_prof.stdout)?;
+
+    let num = CPU_RE.captures(&sys_prof)
+        .and_then(|cap| cap.get(0))
+        .ok_or_else(|| failure::err_msg("couldn't find a CPU core number from `system_profiler SPHardwareDataType`"))?;
+    let num = num.as_str().parse::<usize>()?;
+    Ok(num)
 }
 
 fn main() -> io::Result<()> {
@@ -67,9 +97,8 @@ fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8081")?;
     for stream in listener.incoming() {
         let s = stream?;
-        let tx = log_tx.clone();
         pool.execute(move || {
-            handle_client(s, &tx)
+            handle_client(s, &log_tx)
                 .and_then(|_| {
                     eprintln!("close connection");
                     Ok(())
